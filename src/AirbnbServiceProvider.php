8f1201158e741df66f9b1ad067d48305ecd22406
andrew_marhai <buk2018irinam@gmail.com>
2021-09-05 19:39:47 +0300

    Added the AirbnbServiceProvider.php

---
+++ src/AirbnbServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Amarkhai\AirbnbSdk;
+
+use Illuminate\Support\ServiceProvider;
+
+class AirbnbServiceProvider extends ServiceProvider
+{
+    /**
+     * Publish the configuration file
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/airbnb.php' => config_path('airbnb.php'),
+        ]);
+    }
+}
